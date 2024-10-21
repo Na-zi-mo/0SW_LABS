ae080d90de31ae3eedb5d3fd5a9826d3f87e7ef6
nazimadda <nazim.adda@fmxsolutions.com>
2024-10-21 14:18:55 -0400

    Can Change Direction While Falling and Jumping

--- tp3/player/fall_state.gd
+++ tp3/player/fall_state.gd
@@ -4,12 +4,25 @@ class_name PlayerFall
 @export var player : Player
 var state_machine : AnimationNodeStateMachinePlayback 
 	
+@export var ACCEL = 75.0
+	
 func enter():
 	state_machine = player.get_state_machine()
+
+func manage_input() -> int:	
+	var dir = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")	
+	return dir
 	
 func update(delta : float) -> void:
 	if not state_machine:
 		state_machine = player.get_state_machine()
+	
+	var dir := manage_input()
+	if dir > 0:
+		player.facing_right = true
+	elif dir < 0:
+		player.facing_right = false
+	player.motion.x = ACCEL * dir
 	
 	if player.is_on_floor() :
 		Transitioned.emit(self, "idle")
--- tp3/player/jump_state.gd
+++ tp3/player/jump_state.gd
@@ -4,17 +4,31 @@ class_name PlayerJump
 @export var player : Player
 var state_machine : AnimationNodeStateMachinePlayback 
 @export var JUMPFORCE = 400
+@export var ACCEL = 75.0
 
-	
+func manage_input() -> int:	
+	var dir = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")	
+	return dir
+
 func enter():
+	
+	
 	state_machine = player.get_state_machine()
 	player.state_machine.travel("jump")
+
 	player.motion.y = -JUMPFORCE
 	
 
 func update(delta : float) -> void:
 	if not state_machine:
 		state_machine = player.get_state_machine()
+	
+	var dir := manage_input()
+	if dir > 0:
+		player.facing_right = true
+	elif dir < 0:
+		player.facing_right = false
+	player.motion.x = ACCEL * dir
 	
 	if player.velocity.y >= 0 :
 		Transitioned.emit(self, "fall")
